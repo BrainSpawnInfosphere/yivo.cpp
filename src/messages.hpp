5a61758accbf8198be117a44adb0521b26baa211
walchko <walchko@users.noreply.github.com>
2023-02-07 22:52:24 -0700

    update pack

--- src/messages.hpp
+++ src/messages.hpp
@@ -1,54 +1,137 @@
+/******************************************************************************
+MIT License
+
+Copyright (c) 2020 Mom's Friendly Robot Company
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+******************************************************************************/
 #pragma once
+#include <stdint.h>
 
 struct ImuA_t {
-    float ax, ay, az;
-    // float temperature;
-    long timestamp;
+  float ax, ay, az;
+  // float temperature;
+  long timestamp;
 };
 
 struct ImuAGT_t {
-    float ax, ay, az;
-    float gx, gy, gz;
-    float temperature;
-    long timestamp;
+  float ax, ay, az;
+  float gx, gy, gz;
+  float temperature;
+  long timestamp;
 };
 
 struct ImuAGMT_t {
-    float ax, ay, az;
-    float gx, gy, gz;
-    float mx, my, mz;
-    float temperature;
-    long timestamp;
+  float ax, ay, az;
+  float gx, gy, gz;
+  float mx, my, mz;
+  float temperature;
+  long timestamp;
 };
 
 struct ImuAGMQPT_t {
-    float ax, ay, az;
-    float gx, gy, gz;
-    float imu_temp;
-    float mx, my, mz;
-    float qw,qx,qy,qz;
-    float pressure;
-    float temperature;
-    long timestamp;
+  float ax, ay, az;
+  float gx, gy, gz;
+  float imu_temp;
+  float mx, my, mz;
+  float qw,qx,qy,qz;
+  float pressure;
+  float temperature;
+  long timestamp;
 };
 
 
 struct ImuAGMQT_t {
-    float ax, ay, az;
-    float gx, gy, gz;
-    float mx, my, mz;
-    float qw,qx,qy,qz;
-    float temperature;
-    long timestamp;
+  float ax, ay, az;
+  float gx, gy, gz;
+  float mx, my, mz;
+  float qw,qx,qy,qz;
+  float temperature;
+  long timestamp;
 };
 
 struct PressureTemperature {
-    float pressure, temperature;
+  float pressure, temperature;
 };
 
 struct Motors4_t {
     uint16_t m0, m1, m2, m3, armed;
 };
+
 struct Motors6_t {
-    uint16_t m0, m1, m2, m3, m4, m5, armed;
+  uint16_t m0, m1, m2, m3, m4, m5, armed;
+};
+
+struct Distance_t {
+  enum Distance_Sensor: uint8_t {
+    LASER=1,
+    ULTRASOUND=2,
+    INFRARED=4,
+    RADAR=8,
+    LIDAR=16,
+    UNKNOWN=32
+  };
+  uint16_t min_distance; // cm
+  uint16_t max_distance; // cm
+  uint16_t distance; // cm
+  uint8_t type;
+  uint8_t id;
+  uint32_t timestamp;
+};
+
+// struct Axis_t {
+//   float x,y,z;
+// };
+// union Axis_t {
+//   float v[3];
+//   uint8_t b[12]; // 4b * 3
+//   struct {
+//     float x,y,z;
+//   };
+// };
+
+struct Axis_t {
+  union {
+    float v[3];
+    uint8_t b[12]; // 4b * 3
+    struct {
+      float x,y,z;
+    };
+  };
+};
+
+struct Heartbeat_t {
+  enum Sensors: uint16_t {
+    AUTOPILOT = 1,
+    ACCEL = 2,
+    GYRO = 4,
+    BAROMETER = 8,
+    TEMPERATURE = 16,
+    RANGE = 32,
+    GPS = 64,
+    BATTERY = 128,
+    CAMERA = 256,
+    VIDEODOMETRY = 1024,
+    COMPASS = 2048
+  };
+  uint8_t id;
+  uint8_t has_autopilot;
+  uint16_t sensors;
+  uint32_t timestamp;
 };
--- src/yivo.hpp
+++ src/yivo.hpp
@@ -29,29 +29,6 @@ SOFTWARE.
 #include "messages.hpp"
 #include "msgids.hpp"
 
-// constexpr int BUFFER_SIZE = 256;
-
-// // Message IDs
-// constexpr int PING      = 10;
-
-// constexpr int IMU_AGMQT = 140;
-// constexpr int IMU_AGMT  = 141;
-// constexpr int IMU_AGT   = 142;
-// constexpr int IMU_AT    = 143;
-// constexpr int MAGNETIC  = 144;
-// constexpr int RANGE     = 145;
-// constexpr int GPS       = 146;
-// constexpr int PRES_TEMP = 147;
-// constexpr int VOLTAGE   = 148;
-
-// constexpr int MOTORS    = 200;
-// constexpr int SET_MOTORS= 201;
-// constexpr int SET_ACCEL_CAL = 202;
-// constexpr int SET_GYRO_CAL  = 203;
-// constexpr int SET_MAG_CAL   = 204;
-// constexpr int SET_TEL_STREAM= 205;
-
-// constexpr int YIVO_ERROR= 250;
 
 union int_t {
   uint16_t b16;
@@ -59,33 +36,69 @@ union int_t {
 };
 
 enum Error {
-    NONE = 0,
-    INVALID_HEADER   = 1,
-    INVALID_LENGTH   = 2,
-    INVALID_CHECKSUM = 4,
-    INVALID_COMMAND  = 8,
-    INVALID_MSGID    = 16,
-    EXCEED_BUFFER    = 32
+  NONE = 0,
+  INVALID_HEADER   = 1,
+  INVALID_LENGTH   = 2,
+  INVALID_CHECKSUM = 4,
+  INVALID_COMMAND  = 8,
+  INVALID_MSGID    = 16,
+  EXCEED_BUFFER    = 32
 };
 
 
 uint8_t checksum(uint16_t payload_size, uint8_t msgid, uint8_t *data);
 
-// template<uint32_t size>
-// struct Buffer {
-//   union {
-//     float    f[size];   // float
-//     uint32_t l[size];   // long
-//     uint16_t s[size*2]; // short
-//     uint8_t  b[size*4]; // byte
-//   };
+// class Yivo;
 
-//   void clear() {
-//     memset(b, 0, size);
-//   }
+
+// class ArduinoSerial {
+//   public:
+//   ArduinoSerial(Yivo* y, Stream* s): yivo(y), stream(s) {}
+
+//     // inline Error pack_n_send(uint8_t msgid) {
+//     //     // return pack_n_send(msgid, nullptr, 0);
+//     //     return pack_n_send(msgid, this->buff, this->payload_size);
+//     // }
+
+//     Error pack_n_send(uint8_t msgid, uint8_t *data, uint16_t sz) {
+//       Error err = yivo->pack(msgid, data, sz);
+//       if (err == Error::NONE) {
+//         stream->write(buff, sz + 6); // FIXME: be able to redefine serial
+//       }
+//       return err;
+//     }
+
+//     /*
+//     Reads in a packet from a serial connection.
+
+//     Returns: message ID
+//     */
+//     uint8_t read_packet() {
+//       int retry = 16;
+//       while (retry-- > 0) {
+//         if (stream.available() > 5) {
+//           buff[0] = buff[1];
+//           buff[1] = stream->read();
+//           if ((buff[0] == this->h0) && (buff[1] == this->h1)) {
+//             buff[2] = stream->read(); // low byte
+//             buff[3] = stream->read(); // high byte
+//             uint16_t payload_size = (buff[3] << 8) + buff[2];
+//             buff[4] = stream->read(); // msgid
+//             stream.readBytes(reinterpret_cast<char*>(&buff[5]), payload_size); // payload
+//             buff[5+payload_size] = stream->read(); // checksum
+//             return buff[4]; // msgid
+//           }
+//         }
+//         else delay(10);
+//       }
+
+//       return 0;
+//     }
+
+//     Yivo* yivo;
+//     Stream* stream;
 // };
 
-// template<typename T, uint32_t size>
 template<uint16_t BUFFER_SIZE>
 class Yivo {
     public:
@@ -97,9 +110,10 @@ class Yivo {
     data: buffer containing data to be sent
     sz: size of data to be sent, sz < (BUFFER_SIZE - 6)
     */
-    Error pack(uint8_t msgid, uint8_t *data, uint16_t sz) {
+    int pack(uint8_t msgid, uint8_t *data, uint16_t sz) {
         if (sz+6 > BUFFER_SIZE) {
-            return Error::EXCEED_BUFFER;
+            // return Error::EXCEED_BUFFER;
+            return -1;
         }
 
         memset(this->buff, 0, BUFFER_SIZE);
@@ -120,7 +134,8 @@ class Yivo {
         this->buff[5+sz] = cs; // off by 1?
 
         this->payload_size = sz;
-        return Error::NONE;
+        // return Error::NONE;
+        return sz + 6;
     }
 
     template<typename T>
@@ -138,17 +153,17 @@ class Yivo {
     }
 
 #ifdef Arduino_h
-    inline Error pack_n_send(uint8_t msgid) {
+    inline int pack_n_send(uint8_t msgid) {
         // return pack_n_send(msgid, nullptr, 0);
         return pack_n_send(msgid, this->buff, this->payload_size);
     }
 
-    Error pack_n_send(uint8_t msgid, uint8_t *data, uint16_t sz) {
-      Error err = pack(msgid, data, sz);
-      if (err == Error::NONE) {
-        Serial.write(buff, sz + 6);
+    int pack_n_send(uint8_t msgid, uint8_t *data, uint16_t sz) {
+      int size = pack(msgid, data, sz);
+      if (size != -1) {
+        Serial.write(buff, size);
       }
-      return err;
+      return size;
     }
 
     /*
